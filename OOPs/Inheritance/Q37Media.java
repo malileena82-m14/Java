d19126d5ccd4ede24aaa40aef7db07139d129c14
Leena Mali <malileena82@gmail.com>
2026-07-15 22:35:58 +0530

    java program

---
+++ OOPs/Inheritance/Q37Media.java
@@ -0,0 +1,36 @@
+/*Question 37: Create a base class Media with attribute platformName.
+Create a child class Subscription that adds baseCost.
+Create another child class OTTSubscription that adds addonCost.
+
+Use this keyword and constructor chaining.
+Store 2 subscriptions using array and calculate total cost.
+Asked In Practice Assignment
+Description
+Shows OTT subscription model using this keyword and chaining.
+Input
+Enter number of Subscriptions: 2
+
+Enter Platform: Netflix
+Enter Base Cost: 500
+Enter Addon Cost: 200
+
+Enter Platform: Hotstar
+Enter Base Cost: 400
+Enter Addon Cost: 100
+Output
+Platform: Netflix, Total Cost: 700
+Platform: Hotstar, Total Cost: 500*/
+
+import java.util.*;
+class Media
+{
+}
+class Subscription extends Media
+{
+}
+class OTTSubscription extends Subscription
+{
+}
+class Q37Media
+{
+}
