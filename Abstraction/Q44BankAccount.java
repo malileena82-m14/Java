9b22a72a17b693ddd6870bd3c9ffb2bffdd95d35
Leena Mali <malileena82@gmail.com>
2026-07-23 15:41:51 +0530

    java program

--- Abstraction/Q44BankAccount.java
+++ Abstraction/Q44BankAccount.java
@@ -25,13 +25,92 @@ Final Balance: 1200*/
 import java.util.*;
 abstract class BankAccount
 {
+	int balance;
+	BankAccount(int balance)
+	{
+		this.balance = balance;
+	}
+	abstract void deposit(int amount);
+	abstract void withdraw(int amount);
+	void display()
+	{
+		System.out.println("Final Balance: "+balance);
+	}
 }
 class SavingsAccount extends BankAccount
 {
+	SavingsAccount(int balance)
+	{
+		super(balance);
+	}
+	void deposit(int amount)
+	{
+		balance = balance + amount;
+	}
+	void withdraw(int amount)
+	{
+		if(balance-amount>=500)
+		{
+			balance = balance - amount;
+		}
+		else
+		{
+			System.out.println("Minimum balance should be 500");
+		}
+	}
+	
 }
 class CurrentAccount extends BankAccount
 {
+	CurrentAccount(int balance)
+	{
+		super(balance);
+	}
+	void deposit(int amount)
+	{
+		balance = balance+amount;
+	}
+	void withdraw(int amount)
+	{
+		balance = balance-amount;
+	}
 }
 class Q44BankAccount
 {
+	public static void main(String x[])
+	{
+		Scanner xyz = new Scanner(System.in);
+		System.out.println("1.Saving balance");
+		System.out.println("2.Current balance");
+		System.out.println("Enter choice");
+		int choice = xyz.nextInt();
+		
+		System.out.println("Enter initial balance");
+		int balance = xyz.nextInt();
+		System.out.println("Enter Deposit");
+		int deposit = xyz.nextInt();
+		System.out.println("Enter withdraw");
+		int withdraw = xyz.nextInt();
+		
+		BankAccount b;
+		switch(choice)
+		{
+			case 1:
+				b = new SavingsAccount(balance);
+				b.deposit(deposit);
+				b.withdraw(withdraw);
+				b.display();
+				break;
+				
+			case 2:
+				b = new CurrentAccount(balance);
+				b.deposit(deposit);
+				b.withdraw(withdraw);
+				b.display();
+				break;
+				
+			default:
+				System.out.println("Invalid choice");
+		}
+	}
 }
---
+++ Day 9/Day9Longest.java
@@ -0,0 +1,7 @@
+/*82. Find the longest repeating subsequence.*/
+
+import java.util.*;
+class Day9Longest
+{
+}
+
